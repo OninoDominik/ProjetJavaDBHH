82d95550722879bc74ca2ce59ad4f7ad0a834094
OninoDominik <grobecdo@gmail.com>
2019-05-29 14:08:18 +0200

    modification front

--- Projet/src/main/java/fr/cci/ProjetJava/SimVille/Projet/controller/VilleController.java
+++ Projet/src/main/java/fr/cci/ProjetJava/SimVille/Projet/controller/VilleController.java
@@ -14,6 +14,7 @@ import fr.cci.ProjetJava.SimVille.Projet.model.repository.VilleRepository;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @Controller
 @RequestMapping(path="/ville")
