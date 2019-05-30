a7b8960c8c494ad550305da494f861c8cd972ffd
Dom Renju <zyroprime@laposte.net>
2019-05-30 12:01:57 +0200

    toutes les pages sont mise en formes

--- Projet/src/main/java/fr/cci/ProjetJava/SimVille/Projet/controller/VilleController.java
+++ Projet/src/main/java/fr/cci/ProjetJava/SimVille/Projet/controller/VilleController.java
@@ -128,7 +128,7 @@ public class VilleController {
             tailletoString += "px";
             model.addAttribute("taille", tailletoString);
             model.addAttribute("Listeurl", ListeUrl);
-            return "Carte3";  // on utilise thymeleaf -> retourne al page Accueil.html du dossier ressources
+            return "cartedegrade";  // on utilise thymeleaf -> retourne al page Accueil.html du dossier ressources
 
         }
     }
